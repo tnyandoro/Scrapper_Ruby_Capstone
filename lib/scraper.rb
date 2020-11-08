d257b94e9a41891eb0769da056b94b75467f3f15
Tendai Nyandoro <tendai1@hotmail.com>
2020-11-08 23:43:36 +0200

    Add a method to read the HTML with nokogiri to aid extraction of data

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ require 'byebug'
 def scraper
    url = "https://www.infoisinfo.co.za/search/solar-systems/"
    unparsed_page = HTTParty.get(url)
+   parsed_page = Nokogiri::HTML(unparsed_page)
    byebug
 end
 
