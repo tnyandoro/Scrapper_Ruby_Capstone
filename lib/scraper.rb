80bd0f8ae2e9f7068345f3e827622d6f1ca84186
Tendai Nyandoro <tendai1@hotmail.com>
2020-11-09 01:33:08 +0200

    Add method to iterate through the company information

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,22 @@ require 'pry'
 require 'byebug'
 
 def scraper
-   url = "https://www.infoisinfo.co.za/search/solar-systems/"
-   unparsed_page = HTTParty.get(url)
-   parsed_page = Nokogiri::HTML(unparsed_page)
-   byebug
+  url = 'https://themanifest.com/za/software-development/companies/'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  company_collect = []
+  companies = parsed_page.css('article.company-list-items') # 29 companies
+  companies.each do |company_list|
+    company = {
+      name: company_list.css('span').text,
+      service: company_list.css('field--name-field-pp-company-description-tm').text,
+      project: company_list.css('field--label').text,
+      url: company_list.css('a')[0].attributes['href'].value
+
+    }
+    company_collect << company
+  end
+  byebug
 end
 
 scraper
